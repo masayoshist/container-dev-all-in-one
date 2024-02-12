35e4cacd915c0612182a606f38ee5c8872b831f3
masayoshist masayo <57345418+masayoshist@users.noreply.github.com>
2024-02-12 22:01:59 +0900

    Update setup1.sh
    add wheel nopass

--- setup/setup1.sh
+++ setup/setup1.sh
@@ -9,4 +9,6 @@ systemctl disable firewalld
 setenforce 0
 sed -i s/SELINUX=enforcing/SELINUX=disabled/ /etc/selinux/config
 
+sed -i "s/^%wheel.*/%wheel ALL=\(ALL\) NOPASSWD:ALL/" /etc/sudoers
+
 echo "Please Reboot and Execute setup2.sh"
